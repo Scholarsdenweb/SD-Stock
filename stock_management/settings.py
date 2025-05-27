26b03d353ec27652e4653108dff0c4e8e19b2181
Scholarsdenweb <webdeveloper@scholarsden.in>
2025-05-27 11:29:07 +0530

    added static

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -132,13 +132,16 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'statticfiles')
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 MEDIA_URL = 'media/'
 MEDIA_ROOT = '/media/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
+    "/SD-stock/static/",
+    "/SD-stock/staticfiles/",
+    
 ]
 
 # Default primary key field type
