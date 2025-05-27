896078b61572d52e5dd64b0c748a363d04b7cbee
Scholarsdenweb <webdeveloper@scholarsden.in>
2025-05-27 11:45:32 +0530

    added static3

--- stock_management/settings.py
+++ stock_management/settings.py
@@ -137,12 +137,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 MEDIA_URL = 'media/'
 MEDIA_ROOT = '/media/'
 
-# STATICFILES_DIRS = [
-#     BASE_DIR / "static",
-#     "/SD-stock/static/",
-#     "/SD-stock/staticfiles/",
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
     
-# ]
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
